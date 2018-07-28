e5c1027bf1e8be8d06c5350b7420c641d389ff46
nikitadavydov <aweq4302wer@gmail.com>
2018-07-28 20:24:52 +0300

    update

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
 $('#oneimg').click(function () {
-	$('#main').css("background-image","url(img/dc5e4678faf3.jpg)")
+	$('#main').removeAttr('src')
+	$('#main').attr('src','img/dc5e4678faf3.jpg')
 	$('.picter').removeClass('on')
 	$('#oneimg').toggleClass('on')
 })
 $('#twoimg').click(function () {
-	$('#main').css("background-image","url(img/19f33db20858d703009e1a8040fd946e.jpeg)")
+	$('#main').removeAttr('src')
+	$('#main').attr('src','img/19f33db20858d703009e1a8040fd946e.jpeg')
 	$('.picter').removeClass('on')
 	$('#twoimg').toggleClass('on')
+
 })
 $('#threeimg').click(function () {
-	$('#main').css("background-image","url(img/20161104_123413.jpg)")
+	$('#main').removeAttr('src')
+	$('#main').attr('src','img/20161104_123413.jpg')
 	$('.picter').removeClass('on')
 	$('#threeimg').toggleClass('on')
 })
 $('#fourimg').click(function () {
-	$('#main').css("background-image","url(img/20161203_143012.jpg)")
+	$('#main').removeAttr('src')
+	$('#main').attr('src','img/20161203_143012.jpg')
 	$('.picter').removeClass('on')
 	$('#fourimg').toggleClass('on')
 })
 $('#fiveimg').click(function () {
-	$('#main').css("background-image","url(img/20180416.jpg)")
+	$('#main').removeAttr('src')
+	$('#main').attr('src','img/20180416.jpg')
 	$('.picter').removeClass('on')
 	$('#fiveimg').toggleClass('on')
 })
 $('#siximg').click(function () {
-	$('#main').css("background-image","url(img/dc5e4678faf3.png)")
+	$('#main').removeAttr('src')
+	$('#main').attr('src','img/dc5e4678faf3.png')
 	$('.picter').removeClass('on')
 	$('#siximg').toggleClass('on')
 })
