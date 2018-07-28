594324e03ad705ea06c80e066b2a08333be8a698
nikitadavydov <aweq4302wer@gmail.com>
2018-07-28 17:17:01 +0300

    update

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 $('#oneimg').click(function () {
 	$('#main').css("background-image","url(img/dc5e4678faf3.jpg)")
+	$('.picter').removeClass('on')
+	$('#oneimg').toggleClass('on')
 })
 $('#twoimg').click(function () {
 	$('#main').css("background-image","url(img/19f33db20858d703009e1a8040fd946e.jpeg)")
+	$('.picter').removeClass('on')
+	$('#twoimg').toggleClass('on')
 })
 $('#threeimg').click(function () {
 	$('#main').css("background-image","url(img/20161104_123413.jpg)")
+	$('.picter').removeClass('on')
+	$('#threeimg').toggleClass('on')
 })
 $('#fourimg').click(function () {
 	$('#main').css("background-image","url(img/20161203_143012.jpg)")
+	$('.picter').removeClass('on')
+	$('#fourimg').toggleClass('on')
 })
 $('#fiveimg').click(function () {
 	$('#main').css("background-image","url(img/20180416.jpg)")
+	$('.picter').removeClass('on')
+	$('#fiveimg').toggleClass('on')
+
 })
 
