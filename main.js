24d5b2a56798049a42dcd82cecde0cb9be5b8cba
nikitadavydov <aweq4302wer@gmail.com>
2018-07-28 16:19:41 +0300

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+$('#oneimg').click(function () {
+	$('#main').css("background-image","url(img/dc5e4678faf3.jpg)")
+})
+$('#twoimg').click(function () {
+	$('#main').css("background-image","url(img/19f33db20858d703009e1a8040fd946e.jpeg)")
+})
+$('#threeimg').click(function () {
+	$('#main').css("background-image","url(img/20161104_123413.jpg)")
+})
+$('#fourimg').click(function () {
+	$('#main').css("background-image","url(img/20161203_143012.jpg)")
+})
+$('#fiveimg').click(function () {
+	$('#main').css("background-image","url(img/20180416.jpg)")
+})
+
