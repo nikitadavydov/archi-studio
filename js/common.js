8a7ea06fbe06b0e9bbdafb920a0418004e7d4117
nikitadavydov <aweq4302wer@gmail.com>
2018-08-22 17:54:49 +0300

    update

--- js/common.js
+++ js/common.js
@@ -40,3 +40,20 @@ function(){
 function(){
   $('.hide').eq(5).css('opacity','0')
 });
+
+$(document).ready(function() {
+
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+	
+});
